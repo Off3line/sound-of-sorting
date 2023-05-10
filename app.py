91dc113d2768ec5a0573d1625fac44c675ad8091
Philipp WSL <johnyten@hotmail.com>
2023-05-10 16:45:08 +0200

    implemented bubbleSort

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+from algos import bubbleSort
+bar_width = 0.25
 
-x = np.linspace(0, 2 * np.pi, 200)
-y = np.sin(x)
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.show()
+        
+# fig, ax = plt.subplots(layout='constrained',Tite)
+
+# ax.set_ylabel(' Y Label')
+# ax.set_xlabel(' X Label')
+# ax.set_title('Sound of Sorting')
+
+
+
+# plt.show()
+
+rnd_length = random.randint(1,100)
+print(rnd_length)
+
+bubble_sort_list = []
+
+for x in range(rnd_length):
+    x = random.randint(1,100)
+    bubble_sort_list.append(x)
+
+print(bubble_sort_list)
+print(len(bubble_sort_list))
+
+bubbleSort(bubble_sort_list)
+print(bubble_sort_list)
