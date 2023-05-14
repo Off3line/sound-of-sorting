54268c21dec57902cd382ef0235ffc30b2f6ec29
Philipp WSL <johnyten@hotmail.com>
2023-05-14 09:23:02 +0200

    now back to init without a function and simple onclick error handling on startup

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ bar_rects = None
 generator = None
 
 
-def btncall(input_str,input_nr,input_ms,ax):
+def btncall(*args):
     if input_str.text_disp._text == 'BubbleSort':
         print('Hello World!')
         print(input_nr.text_disp._text)
@@ -25,9 +25,12 @@ def btncall(input_str,input_nr,input_ms,ax):
         bar_rects = ax.bar(range(len(lst)),lst,align='edge')
         text = ax.text(0.02, 0.95, "", transform=ax.transAxes)
         update_fig(lst,bar_rects,iteration,text,generator)
+    elif input_str.text_disp._text == 'Type':
+        pass
     else:
-     raise Exception('Exception Error wrong input')
-       
+        raise Exception('Exception Error wrong input')
+
+
 def update_fig(A, rects, iteration,text,generator):
     for rect, val in zip(rects, A):
         rect.set_height(val)
@@ -45,31 +48,28 @@ def genRandomNr(qty):
    print(rnd_list)
    return rnd_list
 
+  
 
-def startAlgo(v):
-   pass
+fig, ax = plt.subplots()
+ax.set_ylabel(' Y Label')
+ax.set_xlabel(' X Label')
+ax.set_title('Sound of Sorting')
+ax.text(0.15,-0.35,'Select one Algo: '+ ' '.join(algo_list))
+plt.subplots_adjust(bottom=0.30,left=0.20)
+
+input_nr_ax = fig.add_axes([0.15,0.01,0.13,0.05])
+input_nr = TextBox(input_nr_ax,'Qty Dataset',initial='1')
+
+input_str_ax = fig.add_axes([0.40,0.01,0.13,0.05])
+input_str = TextBox(input_str_ax,'Algo',initial='Type')
+
+input_ms_ax = fig.add_axes([0.6,0.01,0.13,0.05])
+input_ms = TextBox(input_ms_ax,'MS',initial='1000')
+
+bubble_pos = fig.add_axes([0.8,0.01,0.13,0.05])
+bubble_btn = Button(bubble_pos,label='Start',color='red')
+
+bubble_btn.on_clicked(btncall)
 
 
-def initPlot():
-    fig, ax = plt.subplots()
-    ax.set_ylabel(' Y Label')
-    ax.set_xlabel(' X Label')
-    ax.set_title('Sound of Sorting')
-    ax.text(0.15,-0.35,'Select one Algo: '+ ' '.join(algo_list))
-    plt.subplots_adjust(bottom=0.30,left=0.20)
-
-    input_nr_ax = fig.add_axes([0.15,0.01,0.13,0.05])
-    input_nr = TextBox(input_nr_ax,'Qty Dataset',initial='1')
-
-    input_str_ax = fig.add_axes([0.40,0.01,0.13,0.05])
-    input_str = TextBox(input_str_ax,'Algo',initial='BubbleSort')
-
-    input_ms_ax = fig.add_axes([0.6,0.01,0.13,0.05])
-    input_ms = TextBox(input_ms_ax,'MS',initial='1000')
-
-    bubble_pos = fig.add_axes([0.8,0.01,0.13,0.05])
-    bubble_btn = Button(bubble_pos,label='Start',color='red',command=lambda: startAlgo(input_str,input_nr,input_ms,ax))
-   # bubble_btn.on_clicked(btncall)
-    plt.show()
-
-initPlot()
+plt.show()
